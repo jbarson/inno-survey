eb6983be8bfa5ad313c08ba33d5b0dcaf35616af
briwalker <brian.p.walker@gmail.com>
2019-04-18 12:23:36 -0400

    breaking out scss files

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ import * as SurveyVue from "survey-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import * as widgets from "surveyjs-widgets";
 import ciobv from "../src/assets/CIOBV.json";
+import "./assets/styles/base.scss";
+import "./assets/styles/button-group.scss"
 
 const Survey = SurveyVue.Survey;
 Survey.cssType = "bootstrap";
@@ -44,7 +46,6 @@ export default {
 
 <style lang='scss'>
 
-@import url(https://fonts.googleapis.com/css?family=Exo|Montserrat|Roboto);
 
 #app {
   font-family: 'Roboto', Helvetica, Arial, sans-serif;
@@ -55,57 +56,6 @@ export default {
   margin-top: 60px;
 }
 
-.btn-group {
-
-  .btn,
-  .btn-default {
-    background: #f7be44;
-    color: white;
-    border: 2px solid #f7be44;
-    font-weight: 900;
-    font-family: 'Exo', sans-serif;
-    transition: all ease 0.3s;
-
-    &.active, &:focus {
-      box-shadow: inset 0 0 5px rgba(255,255,255,0.5);
-      border-color: blue;
-
-      &:hover {
-        color: white;
-      }
-    }
-  }
-
-  .btn:first-child {
-    background: #671611;
-    border: 2px solid #671611;
-  }
-
-  .btn:nth-child(2) {
-    background: #ae2932;
-    border: 2px solid #ae2932;
-  }
-
-  .btn:nth-child(3) {
-    background: #ce3436;
-    border: 2px solid #ce3436;
-  }
-
-  .btn:nth-child(8) {
-    background: #8fc96f;
-    border: 2px solid #8fc96f;
-  }
-
-  .btn:nth-child(9) {
-    background: #5bb656;
-    border: 2px solid #5bb656;
-  }
-
-  .btn:nth-child(10) {
-    background: #21914c;
-    border: 2px solid #21914c;
-  }
-}
 
 .sv_main {
   background: none;
