7f38082aabcd1e9c1745b5fb4b020db06de58b4e
Michael Rychly <michaelrychly@gmail.com>
2019-04-18 17:24:43 -0400

    adjusted rating to be all alike

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,27 @@
   <div id="app">  
     <div id="surveyElement">
       <img src="./assets/images/itrg-logo.svg" id="logo">
-            <survey :survey='survey'/>
-        </div>
-        <div id="surveyResult">{{survey.data}}</div>
+      <survey :survey='survey'/>
+    </div>
+    <div id="surveyResult">{{survey.data}}</div>
   </div>
 </template>
 
 <script>
-import * as SurveyVue from "survey-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import * as widgets from "surveyjs-widgets";
-import ciobv from "../src/assets/CIOBV.json";
-import "./assets/styles/base.scss";
-import "./assets/styles/typography.scss";
-import "./assets/styles/buttons.scss"
-import "./assets/styles/textarea.scss"
-import "./assets/styles/text-input.scss"
-import "./assets/styles/button-group.scss"
-import "./assets/styles/footer.scss"
-import "./assets/styles/progress-bar.scss"
-import "./assets/styles/alerts.scss"
-import "./assets/styles/start-page.scss"
+  import * as SurveyVue from "survey-vue";
+  import "bootstrap/dist/css/bootstrap.css";
+  import * as widgets from "surveyjs-widgets";
+  import ciobv from "../src/assets/CIOBV.json";
+  import "./assets/styles/base.scss";
+  import "./assets/styles/typography.scss";
+  import "./assets/styles/buttons.scss"
+  import "./assets/styles/textarea.scss"
+  import "./assets/styles/text-input.scss"
+  import "./assets/styles/button-group.scss"
+  import "./assets/styles/footer.scss"
+  import "./assets/styles/progress-bar.scss"
+  import "./assets/styles/alerts.scss"
+  import "./assets/styles/start-page.scss"
 
   const Survey = SurveyVue.Survey;
   Survey.cssType = "bootstrap";
@@ -62,6 +62,5 @@ import "./assets/styles/start-page.scss"
     color: #2c3e50;
     background-color:#fff;
     background-image:linear-gradient(135deg, transparent 0%, #fff 25%, #fff 75%, transparent 100%), url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAIklEQVQoU2N89+7dfwYsQEhIiBEkzDgkFGDzAbIY2Cv4AACvrBgJjYNGfwAAAABJRU5ErkJggg==);
-  
   }
 </style>
